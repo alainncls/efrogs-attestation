e1265d523bd005fd89d496005371fc444593916b
Alain Nicolas <alain.nicolas.perso@gmail.com>
2024-04-22 00:18:15 +0200

    feat: Deploy and register Portal contract

--- contracts/scripts/deployEFrogs.ts
+++ contracts/scripts/deployEFrogs.ts
@@ -1,15 +1,21 @@
-import { ethers, run } from 'hardhat';
+import { ethers, network, run } from 'hardhat';
 import dotenv from 'dotenv';
+import { VeraxSdk } from '@verax-attestation-registry/verax-sdk';
+import { Address, Hex } from 'viem';
 
 dotenv.config({ path: '../.env' });
 
 async function main() {
   console.log(`START EFROGS SCRIPT`);
 
-  const { ROUTER_ADDRESS } = process.env;
+  const { ROUTER_ADDRESS, PRIVATE_KEY } = process.env;
 
   if (!ROUTER_ADDRESS || ROUTER_ADDRESS === '') {
     throw new Error('ROUTER_ADDRESS is not set in .env file');
+  }
+
+  if (!PRIVATE_KEY || PRIVATE_KEY === '') {
+    throw new Error('PRIVATE_KEY is not set in .env file');
   }
 
   console.log('Deploying EFrogsPortal...');
@@ -21,7 +27,7 @@ async function main() {
     constructorArguments,
   );
   await eFrogsPortal.waitForDeployment();
-  const eFrogsPortalAddress = await eFrogsPortal.getAddress();
+  const eFrogsPortalAddress = (await eFrogsPortal.getAddress()) as Address;
 
   await new Promise((resolve) => setTimeout(resolve, 3000));
 
@@ -30,8 +36,32 @@ async function main() {
     constructorArguments: constructorArguments,
   });
 
-  console.log(`EFrogsPortal successfully deployed end verified!`);
+  console.log(`EFrogsPortal successfully deployed and verified!`);
   console.log(`EFrogsPortal is at ${eFrogsPortalAddress}`);
+
+  console.log('Registering EFrogsPortal...');
+
+  const chainId = network.config.chainId;
+  const signers = await ethers.getSigners();
+  const signer = signers[0];
+  const veraxSdk = new VeraxSdk(
+    chainId === 59144
+      ? VeraxSdk.DEFAULT_LINEA_MAINNET
+      : VeraxSdk.DEFAULT_LINEA_TESTNET,
+    signer.address as Address,
+    PRIVATE_KEY as Hex,
+  );
+
+  await veraxSdk.portal.register(
+    eFrogsPortalAddress,
+    'eFrogs Portal',
+    'eFrogs attestations',
+    true,
+    'alainnicolas.eth',
+    true,
+  );
+
+  console.log(`EFrogsPortal is registered!`);
 
   console.log(`END EFROGS SCRIPT`);
 }
