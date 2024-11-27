e5ed331f23c744f33ede5df7716793489d0de7b1
Alain Nicolas <alain.nicolas.perso@gmail.com>
2024-11-27 15:07:51 +0100

    chore: Update Verax SDK + Infura API key

--- packages/frontend/src/wagmiConfig.ts
+++ packages/frontend/src/wagmiConfig.ts
@@ -1,16 +1,24 @@
 import { lineaSepolia, mainnet } from 'wagmi/chains';
 import { getDefaultConfig } from 'connectkit';
-import { createConfig } from 'wagmi';
+import { createConfig, http } from 'wagmi';
 
 export const walletConnectProjectId = 'b90f66826134d75b644e3311789615da';
+const infuraApiKey: string = '2VbuXFYphoB468fyFPinOmis7o5';
+
 const chains = [lineaSepolia, mainnet] as const;
 export const wagmiConfig = createConfig(
   getDefaultConfig({
-    chains,
     walletConnectProjectId,
     appName: 'eFrogs Attestations',
     appDescription: 'Issue attestation of eFrogs ownership',
     appUrl: 'https://efrogs.alainnicolas.fr',
     appIcon: 'https://efrogs.alainnicolas.fr/favicon.jpg',
+    chains,
+    transports: {
+      [mainnet.id]: http(`https://mainnet.infura.io/v3/${infuraApiKey}`),
+      [lineaSepolia.id]: http(
+        `https://linea-sepolia.infura.io/v3/${infuraApiKey}`,
+      ),
+    },
   }),
 );
