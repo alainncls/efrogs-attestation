6ed96dd62398647b7cafb3b4d0ba52b0540bd399
Alain Nicolas <alain.nicolas.perso@gmail.com>
2024-12-10 22:43:38 +0100

    fix: Remove unused Hardhat plugin

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,6 +1,5 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
-import 'hardhat-contract-sizer';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env' });
